92d86d66ee664603cdeec30e8be638cda488cf8c
vagrant <vagrant@ubex>
2021-10-21 22:15:36 +0000

    adding SD reading, CSV, and partial JSON

--- shaking_table_clearcore_mcb.ino
+++ shaking_table_clearcore_mcb.ino
@@ -56,12 +56,15 @@
 
 // Specifies which motor to move.
 // Options are: ConnectorM0, ConnectorM1, ConnectorM2, or ConnectorM3.
-#define motor ConnectorM0
+#define motorX ConnectorM0
+#define motorY ConnectorM1
 
 // Select the baud rate to match the target serial device
 #define baudRate 9600
 
-File myFile;
+File inputCSVFile;
+File configFile;
+File logFile;
 
 // Define the velocity and acceleration limits to be used for each move
 int velocityLimit = 10000; // pulses per sec
@@ -72,7 +75,25 @@ int accelerationLimit = 100000; // pulses per sec^2
 // of the example.
 void MoveDistance(int distance);
 
-const char input[] = "[true, 42, \"apple\"]";
+char * csvStr = "";
+String jsonStr = "[true, 42, \"apple\"]";
+int moveMode = 0; //0: distance and 1: velocity and 2: acceleration
+
+float *xVals; //This is used to save distance or velocity or acceleration at x-axis depends on mode
+float *yVals; //This is used to save distance or velocity or acceleration at y-axis depends on mode
+int32_t *longTimestamp;
+int numOfRecords = 0;
+
+
+//The headline string of the CSV file
+char X_DISTANCE[] = "x_distance";
+char Y_DISTANCE[] = "y_distance";
+char X_VELOCITY[] = "x_velocity";
+char Y_VELOCITY[] = "y_velocity";
+char X_ACCELERATION[] = "x_acceleration";
+char Y_ACCELERATION[] = "y_acceleration";
+char TIMESTAMP[] = "timestamp";
+
 
 void setup() {
     // Put your setup code here, it will only run once:
@@ -85,63 +106,25 @@ void setup() {
         continue; 
     }
 
-    Serial.print("Initializing SD card...");
+    initSD();
+    initLogFile();
+    readCSVFile();
+    CSV_Parser cp(csvStr, /*format*/ "Lff");
 
-    if (!SD.begin()) {
-        Serial.println("initialization failed!");
-        while (true) {
-            // We can't continue without a working SD card
-            continue;
-        }
+    numOfRecords = cp.getRowsCount();
+
+    if (moveMode == 0) {
+        xVals = (float*)cp[X_DISTANCE]; //This is used to save distance or velocity or acceleration at x-axis depends on mode
+        yVals = (float*)cp[Y_DISTANCE];  //This is used to save distance or velocity or acceleration at y-axis depends on mode
+        longTimestamp = (int32_t*)cp[TIMESTAMP];   
     }
-    Serial.println("initialization done.");
-
-    // open the file. note that only one file can be open at a time,
-    // so you have to close this one before opening another.
-    myFile = SD.open("input.txt", FILE_WRITE);
-
-    // If the file opened okay, write to it:
-    if (myFile) {
-        Serial.print("Writing to input.txt...");
-        myFile.println("testing 1, 2, 3.");
-        // Close the file:
-        myFile.close();
-        Serial.println("done.");
-    } 
-    else {
-        // If the file didn't open, print an error:
-        Serial.println("error opening input.txt");
-        while (true) {
-            continue;
-        }
-    }
-
-    // Re-open the file for reading:
-    myFile = SD.open("input.txt");
-    if (myFile) {
-        Serial.println("input.txt:");
-
-        // Read from the file until there's nothing else in it:
-        while (myFile.available()) {
-            Serial.write(myFile.read());
-        }
-        // Close the file:
-        myFile.close();
-    } 
-    else {
-        // If the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-    }
-
-
-
 
    // ---- End of read/write code -----
 
 
    // --- read config json file ----
 
-   demoParse();
+   //demoParse();
    // ---end of json reading
  
     // Sets the input clocking rate.
@@ -159,10 +142,10 @@ void setup() {
     //motor.PolarityInvertSDHlfb(true);
 
     // Sets the maximum velocity for each move
-    motor.VelMax(velocityLimit);
+    motorX.VelMax(velocityLimit);
 
     // Set the maximum acceleration for each move
-    motor.AccelMax(accelerationLimit);
+    motorX.AccelMax(accelerationLimit);
 
     // Sets up serial communication and waits up to 5 seconds for a port to open.
     // Serial communication is not required for this example to run.
@@ -174,7 +157,7 @@ void setup() {
     }
 
     // Enables the motor.
-    motor.EnableRequest(true);
+    motorX.EnableRequest(true);
 
     // Waits for HLFB to assert. Uncomment these lines if your motor has a 
     // "servo on" feature and it is wired to the HLFB line on the connector.
@@ -187,23 +170,44 @@ void setup() {
 
 void loop() {
     // Put your main code here, it will run repeatedly:
+    if (moveMode == 0 ) {
+        for (int i = 0; i < numOfRecords; i++) {
+             Serial.print(longTimestamp[i]);             
+             Serial.print(" - ");
+             Serial.print(xVals[i]);          
+             Serial.print(" - ");
+             Serial.print(yVals[i]);           
+             Serial.print(" - ");
+             MoveXDistance(xVals[i]);
+             MoveXDistance(yVals[i]);
+             if (i+1 < numOfRecords) {
+                 delay(longTimestamp[i+1] - longTimestamp[i]);
+             }
+        }
+    } else if (moveMode == 1) {
+      
+    } else {
+      
+    }
 
-    // Move 6400 counts (positive direction), then wait 2000ms
-    MoveDistance(6400);
-    delay(2000);
-    // Move 19200 counts farther positive, then wait 2000ms
-    MoveDistance(19200);
-    delay(2000);
-    // Move back 12800 counts (negative direction), then wait 2000ms
-    MoveDistance(-12800);
-    delay(2000);
-    // Move back 6400 counts (negative direction), then wait 2000ms
-    MoveDistance(-6400);
-    delay(2000);
-    // Move back to the start (negative 6400 pulses), then wait 2000ms
-    MoveDistance(-6400);
-    delay(2000);
+//    // Move 6400 counts (positive direction), then wait 2000ms
+//    MoveXDistance(6400);
+//    delay(2000);
+//    // Move 19200 counts farther positive, then wait 2000ms
+//    MoveXDistance(19200);
+//    delay(2000);
+//    // Move back 12800 counts (negative direction), then wait 2000ms
+//    MoveXDistance(-12800);
+//    delay(2000);
+//    // Move back 6400 counts (negative direction), then wait 2000ms
+//    MoveXDistance(-6400);
+//    delay(2000);
+//    // Move back to the start (negative 6400 pulses), then wait 2000ms
+//    MoveXDistance(-6400);
+//    delay(2000);
 }
+
+
 
 /*------------------------------------------------------------------------------
  * MoveDistance
@@ -217,16 +221,32 @@ void loop() {
  *
  * Returns: None
  */
-void MoveDistance(int distance) {
+void MoveXDistance(int distance) {
     Serial.print("Moving distance: ");
     Serial.println(distance);
 
     // Command the move of incremental distance
-    motor.Move(distance);
+    motorX.Move(distance);
 
     // Waits for all step pulses to output
     Serial.println("Moving... Waiting for the step output to finish...");
-    while (!motor.StepsComplete()) {
+    while (!motorX.StepsComplete()) {
+        continue;
+    }
+
+    Serial.println("Steps Complete");
+}
+
+void MoveYDistance(int distance) {
+    Serial.print("Moving distance: ");
+    Serial.println(distance);
+
+    // Command the move of incremental distance
+    motorY.Move(distance);
+
+    // Waits for all step pulses to output
+    Serial.println("Moving... Waiting for the step output to finish...");
+    while (!motorY.StepsComplete()) {
         continue;
     }
 
@@ -235,11 +255,80 @@ void MoveDistance(int distance) {
 //------------------------------------------------------------------------------
 
 
+
+void initSD() {
+    Serial.print("Initializing SD card...");
+
+    if (!SD.begin()) {
+        Serial.println("initialization failed!");
+        while (true) {
+            // We can't continue without a working SD card
+            continue;
+        }
+    }
+    Serial.println("initialization done.");
+}
+
+void initLogFile() {
+    logFile = SD.open("info.log", FILE_WRITE);
+
+    // If the file opened okay, write to it:
+    if (logFile) {
+        Serial.print("Starting to write the log...");
+        logFile.println("Starting to write the log...");
+        // Close the file:
+        logFile.close();
+    } else {
+        // If the file didn't open, print an error:
+        Serial.println("ERROR: opening info.log");
+        while (true) {
+            continue;
+        }
+    }
+}
+
+void readCSVFile() {
+    // Re-open the file for reading:
+    inputCSVFile = SD.open("input.csv");
+    if (inputCSVFile) {
+        //Serial.println();
+        writeLog("Startiing to read the input.csv file");
+
+        // Read from the file until there's nothing else in it:
+        while (inputCSVFile.available()) {
+            //Serial.write(inputCSVFile.read());
+            char ltr = inputCSVFile.read();
+            csvStr += ltr;
+        }
+        writeLog(csvStr);
+        // Close the file:
+        inputCSVFile.close();
+    } 
+    else {
+        // If the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+}
+
+void writeLog(String logStr) {
+    logFile = SD.open("info.log", FILE_WRITE);
+
+    // If the file opened okay, write to it:
+    if (logFile) {
+        Serial.println(logStr);
+        logFile.println(logStr);
+        // Close the file:
+        logFile.close();
+    } else {
+        Serial.println("ERROR: writing a log to info.log");
+    }
+}
+
 void demoParse() {
   Serial.println("parse");
   Serial.println("=====");
 
-  JSONVar myArray = JSON.parse(input);
+  JSONVar myArray = JSON.parse(jsonStr);
 
   // JSON.typeof(jsonVar) can be used to get the type of the variable
   if (JSON.typeof(myArray) == "undefined") {
