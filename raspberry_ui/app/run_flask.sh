2f5b3870b29afb958822d2c2f94c3e2d439517a7
hchang <hchang@10.15.98.157>
2023-01-27 17:22:57 -0500

    Adding ulib folder

--- raspberry_ui/app/run_flask.sh
+++ raspberry_ui/app/run_flask.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-export FLASK_APP=app.py
+BASEDIR=$(pwd)
+
+PYTHONPATH=${BASEDIR}
+export PYTHONPATH=${BASEDIR}/util:$PYTHONPATH
+echo ${PYTHONPATH}
+
+#export FLASK_APP=${BASEDIR}/app.py
 export FLASK_DEBUG=1
 
-flask run --host=0.0.0.0
+#flask run --host=0.0.0.0
+python /app/app.py
 
